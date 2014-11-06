77355d725f8ae862e047ab25c265edb731d84582
Laurens Nutma <laurensnutma@gmail.com>
2014-11-06 15:01:35 +0100

    Fix keys size check

--- lib/unit_4_multivers/account.rb
+++ lib/unit_4_multivers/account.rb
@@ -3,14 +3,14 @@ module Unit4Multivers
     def account_info_list(opts = {})
       required = [:database, :fiscal_year]
 
-      opts.fetch(required) { raise ArgumentError, "Missing required options: #{required - opts.keys}" if required-opts.keys > 0 }
+      opts.fetch(required) { raise ArgumentError, "Missing required options: #{required - opts.keys}" if (required-opts.keys).size > 0 }
       get "/#{opts.fetch(:database)}/AccountInfoList/#{opts.fetch(:fiscal_year)}", opts
     end
 
     def account_manager(opts = {})
       required = [:database, :account_manager_id]
 
-      opts.fetch(required) { raise ArgumentError, "Missing required options: #{required - opts.keys}" if required-opts.keys > 0 }
+      opts.fetch(required) { raise ArgumentError, "Missing required options: #{required - opts.keys}" if (required-opts.keys).size > 0 }
       get "/#{opts.fetch(:database)}/AccountManager/#{opts.fetch(:account_manager_id)}", opts
     end
 
