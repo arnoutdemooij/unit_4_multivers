c41cd6b3d88d47084fd80568ed140d7d331cba6e
Arnout de Mooij <arnoutdemooij@gmail.com>
2017-04-22 14:30:31 +0200

    sales invoice summary with optional querystring

--- lib/unit_4_multivers/sales_invoice_summary.rb
+++ lib/unit_4_multivers/sales_invoice_summary.rb
@@ -1,8 +1,9 @@
 module Unit4Multivers
   class Client
 
-    def sales_invoice_summary customer_id, start_date=nil
-      get "/#{database}/SalesInvoiceSummary/#{customer_id}" + (start_date.present? ? "?startDate=#{start_date}" : "")
+    def sales_invoice_summary customer_id, start_date=nil, end_date=nil
+      querystring = { 'startDate' => start_date, 'endDate' => end_date }.compact.map { |param, value| "#{param}=#{value}" }.join('&')
+      get "/#{database}/SalesInvoiceSummary/#{customer_id}" + (querystring.present? ? "?#{querystring}" : '')
     end
   end
 end
